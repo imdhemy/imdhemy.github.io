7d547e274b27f73eb525a7b960416ba0772186bc
Mohamad Eldhemy <imdhemy@gmail.com>
2022-01-13 10:43:39 +0300

    feat: install tailwind css v.3

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
