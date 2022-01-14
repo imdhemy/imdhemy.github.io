62f2cc2d94872337f91aea76ee7aa4b796781d94
imdhemy <imdhemy@users.noreply.github.com>
2022-01-14 13:16:41 +0000

    deploy: d29665a5442536992a6ba3bfa47ef4be34d64a4f

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,24 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require("cssnano");
+
 module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
     require("postcss-nested"),
     require("autoprefixer"),
+    require("postcss-discard-comments"),
+    purgecss({
+      content: [
+        "./_includes/**/*.html",
+        "./_layouts/**/*.html",
+        "./_posts/*.md",
+        "./*.html",
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
     ...(process.env.NODE_ENV == "production"
-        ? [require("cssnano")({ preset: "default" })]
+        ? [cssnano({ preset: "default" })]
         : []),
   ],
 };
