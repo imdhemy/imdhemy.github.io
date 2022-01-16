bedc81e9ae705072463c6c9e2069a839cb0f90c5
imdhemy <imdhemy@users.noreply.github.com>
2022-01-16 09:53:55 +0000

    deploy: da94894ccf1bee37148ab585114f1e9264f1bafc

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,80 @@
     return mobileNavMenu.classList.contains('hidden');
   }
 
+  /**
+   * checks if a mobile device
+   * @returns {boolean}
+   */
+  const isMobile = () => {
+    return window.innerWidth <= 800 ;
+  }
+
+  /**
+   * Checks if the snippet collapsed
+   * @param element
+   * @returns {boolean}
+   */
+  const isSnippetCollapsed = function(element) {
+    return element.classList.contains('collapsed-code');
+  }
+
+  /**
+   * Shows a hidden snippet
+   * @param element
+   */
+  const showSnippet = function(element) {
+    element.classList.remove('collapsed-code');
+    element.classList.add('expanded-code');
+  }
+
+  /**
+   * Hides a shown snippet
+   * @param element
+   */
+  const hideSnippet = (element) => {
+    element.classList.remove('expanded-code');
+    element.classList.add('collapsed-code');
+  }
+
+  /**
+   * Handles the snippet controller
+   * @param event
+   */
+  const codeSnippetHandler = function(event) {
+    event.preventDefault();
+    const target = event.target;
+    const element = event.target.nextElementSibling;
+    if (isSnippetCollapsed(element)) {
+      showSnippet(element);
+      target.innerHTML = '> Hide snippet';
+    } else {
+      hideSnippet(element);
+      target.innerHTML = '> Show snippet';
+    }
+  }
+
+  /**
+   * Prepends a controller to code container
+   * @param element
+   * @param key
+   */
+  const prependControllerToCodeContainers = function (element, key) {
+    element.classList.add('collapsed-code');
+    const controller = document.createElement('a');
+    controller.addEventListener('click', codeSnippetHandler);
+    controller.setAttribute('href', '#');
+    controller.innerHTML = '> Show snippet';
+    controller.classList.add('snippet-controller');
+    element.parentNode.insertBefore(controller, element);
+  }
+
+  if (isMobile()) {
+    const codeContainers = document.querySelectorAll('div.highlighter-rouge');
+    codeContainers.forEach(prependControllerToCodeContainers);
+  }
+
   const menuController = document.getElementById('nav-controller');
   if (menuController) {
     menuController.addEventListener('click', menuHandler);
   }
-
 })();
