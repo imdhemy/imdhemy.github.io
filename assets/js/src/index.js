eb69ef55117f2b0d784681549a78057d6666166f
imdhemy <imdhemy@users.noreply.github.com>
2026-03-07 22:44:13 +0000

    deploy: 06f7e3174cdd1fb2c9dba85721836838ad3d8ba2

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,5 +1,25 @@
-const burgerMenu = require('./burger-menu');
+import { addIcons } from 'ionicons';
+import { defineCustomElement as defineIonIcon } from 'ionicons/components/ion-icon.js';
+import {
+  close,
+  logoGithub,
+  logoLinkedin,
+  logoTwitter,
+  logoYoutube,
+  menuOutline,
+} from 'ionicons/icons';
+import { init as initBurgerMenu } from './burger-menu';
+
+defineIonIcon();
+addIcons({
+  close,
+  'logo-github': logoGithub,
+  'logo-linkedin': logoLinkedin,
+  'logo-twitter': logoTwitter,
+  'logo-youtube': logoYoutube,
+  'menu-outline': menuOutline,
+});
 
 (() => {
-  burgerMenu.init();
+  initBurgerMenu();
 })();
